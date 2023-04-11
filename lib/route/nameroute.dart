d10695a7574340c20da24b1fe3f787ccf3bb1278
katariya dharmesh <katariyadharmesh658@gmail.com>
2023-04-11 15:57:00 +0530

    modifieed

--- lib/route/nameroute.dart
+++ lib/route/nameroute.dart
@@ -2,5 +2,4 @@ class NameRoutes{
   static const String homeScreen = "/homeScreen";
   static const String profileScreen = "/profileScreen";
   static const String addPostScreen = "/addPostScreen";
-  static const String editDataScreen = "/editDataScreen";
 }
